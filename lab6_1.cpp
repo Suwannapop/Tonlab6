11bbf71989995359e5a67ee5b9b69545e6c17386
= <=>
2022-12-09 15:42:17 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -5,20 +5,23 @@ int main(){
     int number ;
     int Evennumber=0 ;
     int Oddnumber=0 ;
-    cout << "Enter an integer: ";
-    cin >> number ;
-    if (number != 0){
-        while(number !=0){
-            cout << "Enter an integer: ";
-            cin >> number ;
-            if (number%2 == 0){
-                Evennumber++;
-            }else {
-                Oddnumber++;
-            } 
-        }
-    }
 
+    do
+    {
+        cout << "Enter an integer: ";
+        cin >> number ;
+        if (number%2 == 0 && number != 0)
+        {
+            Evennumber++;
+        }
+        if (number%2 != 0)
+        {
+            Oddnumber++;
+        }
+        
+        
+    } while (number != 0);
+    
     cout << "#Even numbers = " << Evennumber << endl;
     cout << "#Odd numbers = " << Oddnumber;
     return 0;
