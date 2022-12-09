a21aa79efd4238961d77c5a093a4a6befb40e2e5
= <=>
2022-12-09 15:23:17 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+    int number ;
+    int Evennumber=0 ;
+    int Oddnumber=0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number ;
+    if (number != 0){
+        while(number !=0){
+            cout << "Enter an integer: ";
+            cin >> number ;
+            if (number%2 == 0){
+                Evennumber++;
+            }else {
+                Oddnumber++;
+            } 
+        }
+    }
+
+    cout << "#Even numbers = " << Evennumber << endl;
+    cout << "#Odd numbers = " << Oddnumber;
     return 0;
 }
